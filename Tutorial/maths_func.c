e2987f992f74354ad5e0be14d597f4659c356ff5
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-09-14 21:17:08 +0530

    Create maths_func.c

---
+++ Tutorial/maths_func.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+//   Calculating something something related to Sphere:::::
+
+int main(){
+
+    double radius = 0.0f;
+    double area = 0.0f;
+    double volume = 0.0f;
+    
+    const double PI = 3.14159;
+
+    printf("Enter the radius of Sphere : ");
+    scanf("%lf", &radius);
+
+    area = 4 * PI * pow(radius ,2);
+    volume = (4/3) * PI * pow(radius, 3);
+
+    printf("Area of the Sphere is : %.2lf cm^2\n", area );
+    printf("Volume of the Sphere is : %.2lf cm^3\n", volume );
+    
+    
+    return 0;
+}
