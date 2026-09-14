959c349a70660e675bad363be2a6137acc77c3ab
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-09-14 20:54:40 +0530

    Update mad_libs_game.c

--- Tutorial/mad_libs_game.c
+++ Tutorial/mad_libs_game.c
@@ -14,24 +14,28 @@ int main(){
 
     printf("Enter an adjective (description): ");
     fgets(adjective1 , sizeof(adjective1), stdin);
+    adjective1[strlen(adjective1) - 1] = '\0';
 
     printf("Enter a noun (animal or person): ");
     fgets(noun , sizeof(noun), stdin);
+    noun[strlen(noun) - 1] = '\0';
 
     printf("Enter an adjective (description): ");
     fgets(adjective2 , sizeof(adjective2), stdin);
+    adjective2[strlen(adjective2) - 1] = '\0';
 
     printf("Enter a verb (ending w/ -ing): ");
     fgets(verb , sizeof(verb), stdin);
+    verb[strlen(verb) - 1] = '\0';
 
     printf("Enter an adjective (description): ");
     fgets(adjective3 , sizeof(adjective3), stdin);
+    adjective3[strlen(adjective3) - 1] = '\0';
 
-    printf("%s\n", noun);
-    printf("%s\n", verb);
-    printf("%s\n", adjective1);
-    printf("%s\n", adjective2);
-    printf("%s\n", adjective3);
+    printf("\nToday I went to a %s zoo.\n", adjective1);
+    printf("In an exhibit, I saw a %s.\n", noun);
+    printf("%s was %s and %s!\n", noun, adjective2, verb);
+    printf("I was %s!\n", adjective3);
 
 
     return 0;
