344b90c6bec14c55d248087a7ba24dcbc2589679
Vikas Banjare <vikasbanjare6721@gmail.com>
2026-09-15 14:29:04 +0530

    Create temperature_converter.c

---
+++ temperature_converter.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+
+    char choice = '\0';
+    float F = 0.0f;
+    float C = 0.0f;
+
+    printf("****** TEMPERETURE CONVERTOR ******\n");
+    printf("*** F . for C -> F ***\n");
+    printf("*** C . for F -> C ***\n");
+    
+    printf("Enter your choice: ");
+    scanf("%c", &choice);
+    choice = toupper(choice);
+
+    if(choice == 'F'){
+        printf("Enter F value : ");
+        scanf("%f", &F);
+
+        C = ((32*F) - 32) * (5/9.0);
+        
+        printf("Your F in C is : %.2f", C);
+    }
+
+    else if(choice == 'C'){
+        printf("Enter C value : ");
+        scanf("%f", &C);
+
+        F = (C * (9/5.0) + 32);
+        
+        printf("Your C in F is : %.2f", F);
+    }    
+    
+    else{
+        printf("Invalid input !!!");
+    }
+
+    return 0;
+}
